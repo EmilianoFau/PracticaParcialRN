42dc9492479495f12a37ae4d46917d6c19b5f862
EmilianoFau <emiliano.fau@correo.ucu.edu.uy>
2024-11-24 14:01:54 -0300

    ya esta el add perfecto solo falta el edit q no borre la img

--- practicaParcial/app/add.tsx
+++ practicaParcial/app/add.tsx
@@ -1,6 +1,6 @@
-import { Text, View, TextInput, Button } from "react-native";
+import { Text, View, TextInput, Button, ScrollView, StyleSheet, TouchableOpacity, Platform } from "react-native";
 import { useState } from "react";
-import { useRouter } from "expo-router";
+import { useRouter, Stack } from "expo-router";
 import { addPlanet } from "../app/api";
 
 const AddPlanet = () => {
@@ -29,7 +29,26 @@ const AddPlanet = () => {
     }
   };
 
+  const handleCancel = () => {
+    router.push("/"); 
+  };
+
   return (
+    <ScrollView
+      style={{
+        flex: 1,
+      }}
+    >
+      <Stack.Screen
+        options={{
+          title: "Add Planet",
+          headerStyle: { backgroundColor: "#020127" },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      />
     <View style={{ padding: 16, flex: 1, justifyContent: "center" }}>
       <TextInput
         placeholder="Name"
@@ -85,9 +104,43 @@ const AddPlanet = () => {
           padding: 8,
         }}
       />
-      <Button title="Add planet" onPress={handleAddPlanet} />
-    </View>
+      <TextInput
+        placeholder="Image URL (optional)"
+        value={newPlanet.image}
+        onChangeText={(text) => setNewPlanet({ ...newPlanet, image: text })}
+        style={{
+          marginBottom: 16,
+          borderBottomWidth: 1,
+          borderColor: "#ccc",
+          padding: 8,
+        }}
+      />
+        <View style={styles.buttonContainer}>
+          <Button title="Add Planet" onPress={handleAddPlanet} />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button title="Cancel" onPress={handleCancel} color="red" />
+        </View>    
+      </View>
+    </ScrollView>
   );
 };
 
 export default AddPlanet;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    marginBottom: 8,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+    padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 8,
+  },
+});
